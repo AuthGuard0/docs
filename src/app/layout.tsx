e6da8eec6d6de2c83f7ff8285005f1ca44dbaae0
MaGiXxScripter0 <123148797+MaGiXxScripter0@users.noreply.github.com>
2025-08-05 11:59:40 +0200

    upd docs rep base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default async function RootLayout({ children }: {
           banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/AuthGuard0/docs/tree/main"
           footer={footer}
           editLink={null}
           feedback={{
