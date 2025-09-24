07b637178239b91ab725d8ea320728be80f9a6a8
MaGiXxScripter0 <123148797+MaGiXxScripter0@users.noreply.github.com>
2025-09-24 17:49:53 +0200

    add webhook protection

--- src/app/_meta.js
+++ src/app/_meta.js
@@ -2,5 +2,6 @@ export default {
     index: 'Registering on AuthGuard',
     'creating-revenues': 'Creating Revenues',
     library: 'Library',
+    'webhook-protection': 'Webhook Protection',
     discord: 'Setup Discord Join from OAuth2'
 }
